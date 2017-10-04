619d6059c270c2625e49f0058e1c31c4c32b2ec0
Tatyana Ostrovskaya <ostrtanja@gmail.com>
2017-10-04 23:37:02 +0300

    added first window

---
+++ MainUtility/FirstWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MainUtility
+{
+    public partial class FirstWindow : Window
+    {
+        private String CurrentDir { get; set; }
+
+        public FirstWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+
+            }
+
+        }
+    }
+}
