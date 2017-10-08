284152afd459f148a6a9d1a0324e3d84217c3460
Tatyana Ostrovskaya <ostrtanja@gmail.com>
2017-10-08 14:38:35 +0300

    added new plugin, fixed dynamic plugin listing

--- MainUtility/MainWindow.xaml.cs
+++ MainUtility/MainWindow.xaml.cs
@@ -6,6 +6,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Security.Permissions;
+using System.Threading;
 
 namespace MainUtility
 {
@@ -17,22 +19,59 @@ namespace MainUtility
         private AggregateCatalog catalog;
         private DirectoryCatalog dirCatalog;
         private CompositionContainer _container;
+        private FileSystemWatcher watcher;
+        private String path = Directory.GetCurrentDirectory() + "\\Extensions";
 
         public SearchArguments SearchArgs { get; set; }
 
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
         public MainWindow()
         {
             InitializeComponent();
 
-            //An aggregate catalog that combines multiple catalogs
             catalog = new AggregateCatalog();
-            //Adds all the parts found in the same assembly as the Program class
-            catalog.Catalogs.Add(new AssemblyCatalog(typeof(SearchArguments).Assembly));
-            String path = Directory.GetCurrentDirectory() + "\\Extensions";
-            dirCatalog = new DirectoryCatalog(path);
-            catalog.Catalogs.Add(dirCatalog);
+           
 
-            catalog.Changed += OnExtensionCatalogChanged;
+            watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+           | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+            watcher.Filter = "*.dll";
+            watcher.Changed += new FileSystemEventHandler(OnExtensionCatalogChanged);
+            watcher.Created += new FileSystemEventHandler(OnExtensionCatalogChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnExtensionCatalogChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            watcher.EnableRaisingEvents = true;
+
+            dirCatalog = new DirectoryCatalog(path);
+
+            ComposeExtensions();
+
+                      
+        }
+
+        // Define the event handlers.
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+
+        private void ComposeExtensions ()
+        {
+            catalog = new AggregateCatalog();
+
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(SearchArguments).Assembly));
+            
+            catalog.Catalogs.Add(dirCatalog);
 
             //Create the CompositionContainer with the parts in the catalog
             _container = new CompositionContainer(catalog);
@@ -47,9 +86,22 @@ namespace MainUtility
                 Console.WriteLine(compositionException.ToString());
             }
 
-            FillPluginNamesStackPanel();
 
-                      
+            Dispatcher.BeginInvoke(new ThreadStart(delegate
+            {
+                pluginNamesListBox.Items.Clear();
+
+                foreach (Lazy<IPlugin, IPluginData> i in plugins)
+                {
+                    AddPluginCheckBox(i.Metadata.Extension);
+
+                    //if (i.Metadata.Extension.Equals("txt"))
+                    //{
+                    //    i.Value.FindFilesByParams(new SearchArguments("C:\\Users\\user\\Desktop\\txt_folder\\papka", false, FileAttributes.Archive));
+                    //    FilesList.ItemsSource = i.Value.searchResult;
+                    //}
+                }
+            }));
         }
 
         private void AddPluginCheckBox(string extension)
@@ -59,22 +111,17 @@ namespace MainUtility
 
         private void FillPluginNamesStackPanel ()
         {
-            foreach (Lazy<IPlugin, IPluginData> i in plugins)
-            {
-                AddPluginCheckBox(i.Metadata.Extension);
-
-                //if (i.Metadata.Extension.Equals("txt"))
-                //{
-                //    i.Value.FindFilesByParams(new SearchArguments("C:\\Users\\user\\Desktop\\txt_folder\\papka", false, FileAttributes.Archive));
-                //    FilesList.ItemsSource = i.Value.searchResult;
-                //}
-            }
+            
         }
 
-        void OnExtensionCatalogChanged(object sender, ComposablePartCatalogChangeEventArgs e)
+        private void OnExtensionCatalogChanged(object source, FileSystemEventArgs e)
         {
             Console.WriteLine("Directory modified");
-            dirCatalog.Refresh();            
+            dirCatalog.Refresh();
+
+            ComposeExtensions();
+
+
         }
 
     }
--- PluginDoc/SearcherDoc.cs
+++ PluginDoc/SearcherDoc.cs
@@ -1,23 +1,21 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.Composition;
 using MainUtility;
+using System.IO;
 
-namespace PluginTxt
+namespace PluginDoc
 {
     [Export(typeof(MainUtility.IPlugin))]
-    [ExportMetadata("Extension", "txt")]
-    public class SearcherTxt : MainUtility.IPlugin
+    [ExportMetadata("Extension", "doc")]
+    public class SearcherDoc : MainUtility.IPlugin
     {
         public List<string> searchResult { get; set; }
         private bool _isSearchStoppedByUser { get; set; }
         private SearchArguments _args;
-
-        public SearcherTxt() { }
 
         public bool FindFilesByParams(SearchArguments args)
         {
@@ -33,6 +31,7 @@ namespace PluginTxt
 
             return true;
         }
+
 
         private void SearchDir(string dirPath)
         {
@@ -78,9 +77,9 @@ namespace PluginTxt
         private bool CheckAllSearchParameters(string file, FileAttributes searchAttributes)
         {
             FileAttributes fileAttr = File.GetAttributes(file);
-            if ((fileAttr.CompareTo (searchAttributes) == 0) && ( file != null ))
+            if ((fileAttr.CompareTo(searchAttributes) == 0) && (file != null))
                 return true;
-       
+
             return false;
         }
     }
